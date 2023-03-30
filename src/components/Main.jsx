53e65b4db443b3ed7600bcd1aca505b6daae762f
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-03-30 18:24:37 -0500

    add desing  main component jsx and add in index

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="bg-white flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold text-gray-900 mb-8">Mi sitio web</h1>
+      <nav className="flex justify-center">
+        <button className="bg-gray-800 text-white text-lg font-bold py-2 px-4 mx-2 rounded-full hover:bg-gray-700">
+          Inicio
+        </button>
+        <button className="bg-gray-800 text-white text-lg font-bold py-2 px-4 mx-2 rounded-full hover:bg-gray-700">
+          Servicios
+        </button>
+        <button className="bg-gray-800 text-white text-lg font-bold py-2 px-4 mx-2 rounded-full hover:bg-gray-700">
+          Contacto
+        </button>
+      </nav>
+    </div>
+  );
+}
+
+export default Main;
