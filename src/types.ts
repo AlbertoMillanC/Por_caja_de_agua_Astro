706a5ae1e514212666a8f35730ebd5c41be7ad6e
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-03-31 23:24:39 -0500

    add rutas navbar and components

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface Project {
+	title: string;
+	client: string;
+	description: string;
+	publishDate: string;
+	tags: string[];
+	img: string;
+}
+
+export interface cardBuy1 {
+	title: string;
+	client: string;
+	description: string;
+	publishDate: string;
+	tags: string[];
+	img: string;
+}
+
+export interface blog {
+	title: string;
+	client: string;
+	description: string;
+	publishDate: string;
+	tags: string[];
+	img: string;
+}
+
